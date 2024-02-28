1227aefce7b3ec0f333d713e6a99660a5dee16bb
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2024-02-28 15:30:20 -0300

    studies with jar dependencies

---
+++ depenjar/src/marcosoft/oca/depenjar/DepenJar.java
@@ -0,0 +1,14 @@
+package marcosoft.oca.depenjar;
+
+public class DepenJar{
+
+	public static void main(String[] args){
+		System.out.println("This is a dependency that comes from a Jar file");
+	}
+
+
+	public int getValue(){
+		return 10;
+	}
+
+}
--- program/src/marcosoft/oca/App.java
+++ program/src/marcosoft/oca/App.java
@@ -4,17 +4,24 @@ import marcosoft.oca.Guitar;
 
 import marcosoft.oca.depen.SomeDependency;
 
+import marcosoft.oca.depenjar.DepenJar;
+
 public class App{
 	public static void main(String[] args){
 		System.out.println("Marcosoft Program");
-
 		Guitar myGuitar = new Guitar();
-
 		myGuitar.play();
 
-		SomeDependency myDependency = new SomeDependency();
 
+		// dependency from another class
+		SomeDependency myDependency = new SomeDependency();
 		myDependency.doSomething();
+
+
+		// dependency from jar file
+		DepenJar myDepenJar = new DepenJar();
+		int valueFromDependency = myDepenJar.getValue();
+		System.out.println("The value that came from Jar dependency is " + valueFromDependency);
 
 	}
 }
