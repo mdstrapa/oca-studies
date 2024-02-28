60889dd81394d106d3278f7e2ac83314d797ebf2
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2024-02-28 16:13:27 -0300

    studies with code block on object instantiation

---
+++ section_2_class_7/App.java
@@ -0,0 +1,16 @@
+public class App{
+	
+	private String appVersion = "1.0.0";
+
+
+	{
+		System.out.println("This block gets executed prior the code inside the constructor");
+	}
+
+	public App(){
+		System.out.println("Here is the constructor");
+	}
+
+
+
+}
---
+++ section_2_class_7/Program.java
@@ -0,0 +1,16 @@
+public class Program{
+
+
+	public static void main(String[] args){
+
+
+		System.out.println("Program starts here!");
+
+
+		App myApp = new App();
+
+		
+
+	}
+
+}
