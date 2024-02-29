3b6716ca8a8b96def932cde43947b28cb71f9f79
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2024-02-29 18:16:12 -0300

    playing with unary operators

---
+++ section_2_class_11/TypeInferLab.java
@@ -0,0 +1,11 @@
+public class TypeInferLab{
+
+
+	var abc = 10;
+
+	void myMethod(){
+		var i = 0;
+		System.out.println("The result is: " + (i + 1));
+	}
+
+}
---
+++ section_3_class_14/OperatorLab.java
@@ -0,0 +1,25 @@
+package marcosoft.oca;
+
+public class OperatorLab{
+
+	public static void main(String[] args){
+
+
+		int a = 6;
+
+		System.out.println("The value of a++ is: " + a++); // it's suppoused to write 6
+
+		System.out.println("The value of ++a is: " + ++a); // it's suppoused to write 8
+
+
+		int b = -7;
+
+		b = -b;
+
+		System.out.println("The value of b is: " + b); // it's suppoused to write 7 positive
+
+
+	}
+
+
+}
