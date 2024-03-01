513435f42bd38afb75168a4ae698ae4e3d144568
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2024-02-29 21:37:32 -0300

    playing with operators

---
+++ section_3_class_15/BinaryOperatorLab.java
@@ -0,0 +1,11 @@
+public class BinaryOperatorLab{
+	
+	public static void main(String[] args){
+
+		short a = 10;
+		short b = 15;
+		short c = a + b;
+
+		System.out.println("The value of C is: " + c);
+	}
+}
